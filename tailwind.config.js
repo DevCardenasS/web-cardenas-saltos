436073472fc77404c9c5a14754638ce78d45cbae
DevCardenasS <developer@cardenassaltos.com>
2026-01-19 02:01:03 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Montserrat"', 'sans-serif'],
+      },
+      colors: {
+        navy: '#0A1D37',
+        gold: '#D4AF37', 
+      }
+    },
+  },
+  plugins: [],
+}

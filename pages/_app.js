2caefcbb2f311e37e94aeab04da2609218ade9dc
DevCardenasS <developer@cardenassaltos.com>
2026-01-19 14:06:36 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.css';
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 

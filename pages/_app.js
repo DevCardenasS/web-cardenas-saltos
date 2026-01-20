87b0b4346fee56866c1948e3ee88c8d47e37591d
DevCardenasS <developer@cardenassaltos.com>
2026-01-20 12:34:16 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,10 @@ import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
+  // Pasamos Component.transparentNavbar como una prop al hijo del Layout
   return (
     <Layout>
-      <Component {...pageProps} />
+      <Component {...pageProps} transparentNavbar={Component.transparentNavbar} />
     </Layout>
   );
 }

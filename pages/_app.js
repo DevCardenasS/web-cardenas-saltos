c8378a5283ae03ee28734e3cd931bcdd8abf31f3
DevCardenasS <developer@cardenassaltos.com>
2026-01-20 16:55:53 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,16 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-import Head from 'next/head'; // 1. Importación necesaria agregada
+import Head from 'next/head'; 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* 2. El bloque Head debe ir dentro del return y envuelto en fragmentos <> */}
       <Head>
         <title>Cárdenas Saltos | Abogados</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.png" /> 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      
+
       <Layout>
         <Component {...pageProps} transparentNavbar={Component.transparentNavbar} />
       </Layout>

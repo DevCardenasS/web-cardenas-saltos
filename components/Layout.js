921bde3af19f6232b30eb435291a2ab10bf86b7a
DevCardenasS <developer@cardenassaltos.com>
2026-01-20 12:33:19 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,13 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
+  // Buscamos si el componente hijo tiene la propiedad de navbar transparente
+  const isTransparent = children.props?.transparentNavbar || false;
+
   return (
     <div className="min-h-screen bg-white text-cs-navy">
-      <Navbar />
+      {/* Pasamos la propiedad al Navbar global */}
+      <Navbar transparent={isTransparent} />
       <main>{children}</main>
       <Footer />
     </div>

458b501b06d74b6cb9ad99248ed3e5e383cc9417
DevCardenasS <developer@cardenassaltos.com>
2026-01-21 17:18:59 -0500

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 // constants.js
-export const WHATSAPP_NUMBER = "593900000000"; // <--- AQUÍ PON EL NUEVO NÚMERO
+export const WHATSAPP_NUMBER = "593959617307"; // <--- AQUÍ PON EL NUEVO NÚMERO
 // Importante: Sin el "+", solo el código de país y el número.

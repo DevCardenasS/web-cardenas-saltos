dfa8e716a19211f69fce123aa18cd2ee8b845074
DevCardenasS <developer@cardenassaltos.com>
2026-01-27 18:25:29 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,27 +2,50 @@ import '../styles/globals.css';
 import Layout from '../components/Layout';
 import Head from 'next/head'; 
 import WhatsAppButton from '../components/WhatsAppButton';
-import { WHATSAPP_NUMBER } from '../constants'; // <-- Importamos la constante aquí también
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Cárdenas Saltos | Abogados</title>
-        {/* Favicon global con la ruta correcta */}
         <link rel="icon" href="/Logos/Favicon-cardenas-saltos-abogados.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
+      {/* Cargamos el script de Google de forma eficiente */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17896545955"
+        strategy="afterInteractive"
+      />
+      
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-17896545955');
+
+          // Función robusta para reportar la conversión
+          window.reportWhatsAppClick = function() {
+            if (typeof gtag === 'function') {
+              gtag('event', 'conversion', {
+                'send_to': 'AW-17896545955/TU_LABEL_AQUÍ',
+                'event_category': 'WhatsApp',
+                'event_label': window.location.pathname, // Nos dice la página: /losep, /administrativo, etc.
+                'value': 1.0,
+                'currency': 'USD'
+              });
+              console.log("Conversión enviada desde: " + window.location.pathname);
+            }
+          };
+        `}
+      </Script>
+
       <Layout>
-        {/* Pasamos Component.transparentNavbar como prop al Layout si es necesario */}
         <Component {...pageProps} transparentNavbar={Component.transparentNavbar} />
       </Layout>
 
-      {/* Colocamos el botón aquí para que aparezca en TODAS las páginas.
-          Al estar centralizado, el componente WhatsAppButton interno 
-          ya debería estar leyendo de constants.js 
-      */}
       <WhatsAppButton />
     </>
   );
